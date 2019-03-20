282f0b425e88e0e229471fa37df9d0ebb924973b
Dmitry-Raskovalov <dmitry.raskovalovv@gmail.com>
2019-03-20 17:18:13 +0700

    init.sh и nginx.conf сконфигурированы для guincorn. hello.py парсит и возвращает строку с параметрами запроса

---
+++ hello.py
@@ -0,0 +1,8 @@
+#Возвращает строку из параметров запроса разделенных символом конца строки
+def application(env, start_response):
+	params = env['QUERY_STRING'].split('&')
+	resp = b''
+	for each in params:
+		resp += (each + '\n').encode('utf-8')
+	start_response('200 OK', [('Content-Type', 'text/plain')])
+	return [resp]
